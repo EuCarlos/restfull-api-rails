09450991f4102338290ec4b1b660b17eef6a3167
Carlos Alve <carlosalves369m@hotmail.com>
2022-08-19 11:35:37 -0300

    :seedling: update seed file
    Using the Faker gem was seeding 20 books, seeding 10 users and seeding 100 reviews

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "➕ Seeding 20 books"
+
+20.times do |n|
+  Book.create! title: Faker::Book.title,
+    author: Faker::Book.author,
+    image: Faker::Avatar.image
+end
+
+p "➕ Seeding 10 users"
+10.times do |n|
+  User.create! email: Faker::Internet.email,
+    password: "test_user"
+end
+
+p "➕ Seeding 100 reviews"
+100.times do |n|
+  Review.create! title: "best book ever",
+    content_rating: Faker::Number.between(from: 1, to: 10),
+    recommend_rating: Faker::Number.between(from: 1, to: 10),
+    user_id: Faker::Number.between(from: 1, to: 10),
+    book_id: Faker::Number.between(from: 1, to: 20)
+end
